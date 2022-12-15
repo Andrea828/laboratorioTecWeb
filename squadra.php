c1f5ca9dba0ab768158c976c3a165a669ba3506c
Andrea Meneghello <andreameneghello828@gmail.com>
2022-12-15 17:40:29 +0100

    update squadra.php

--- squadra.php
+++ squadra.php
@@ -1,16 +1,30 @@
 <?php
 use DB\DBAccess;
 
-require_once "..". DIRECTORY_SEPARATOR ."connessione.php";
+require_once "..". DIRECTORY_SEPARATOR ."connessione.php"; //importo DBAccess dentro connessione.php
 
 $paginaHTML = file_get_contents("squadrea.html");
-
 $connessione = new DBAccess();
 $stringaGiocatori = "";
+$giocatori = "";
 $connOk = $connessione->openDBConnection();
 
 if($connOk) {
-    $stringaGiocatori = "";
+    $giocatori = $connessione->getList();
+    $connessione->closeConnection();
+
+    if ($giocatori != null) {
+        $stringaGiocatori .= '<dl id="giocatori">';
+
+        foreach ($giocatori as $giocatore) {
+            // creare i vari dt e dd
+        }
+
+        $stringaGiocatori .= '</dl>';
+    }
+    else {
+        $stringaGiocatori = "<p>Nessun giocatore presente.</p>";
+    }
 } else {
     $stringaGiocatori = "<p>I sistemi sono momantanemanete fuori servizio, ci scusiamo per il disagio.</p>";
 }
